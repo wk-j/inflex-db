644b92c620d0a1556606801a200f82fd315d7d3c
wk-j <somnuk.wk@gmail.com>
2019-05-07 16:18:47 +0700

    Initial commit

---
+++ src/Db/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using IntelligenceEntity;
+
+namespace Db {
+    class Program {
+        static void Main(string[] args) {
+            var connectionString = "Server=192.168.0.105;Port=5432;Database=IntelligenceCapture; User Id=postgres;Password=abcABC123;";
+
+            var options = new DbContextOptionsBuilder().UseNpgsql(connectionString).Options;
+            using (var context = new IntelligenceEntity.IntelligenceContext(options)) {
+                var outputs = context.Outputs.Where(x => x.Status == OutputStatus.Unclassified);
+                foreach (var item in outputs) {
+                    Console.WriteLine(item.Id);
+                }
+            }
+        }
+    }
+}
